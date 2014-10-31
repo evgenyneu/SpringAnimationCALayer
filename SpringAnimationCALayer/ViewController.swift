e10a12a6cb6c5bfd587899597e4e62e70b696115
Evgenii Neumerzhitckii <evgeny@bikeexchange.com.au>
2014-11-01 09:18:59 +1100

    Initial commit

--- SpringAnimationCALayer/ViewController.swift
+++ SpringAnimationCALayer/ViewController.swift
@@ -9,16 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+  @IBOutlet weak var viewOneContainer: UIView!
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+
+
+    let objectOne = UIView(frame: CGRect(origin: CGPoint: size: CGSize(x: 50, y: 50))
+//    viewOneContainer
   }
 
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
+
 
 
 }
