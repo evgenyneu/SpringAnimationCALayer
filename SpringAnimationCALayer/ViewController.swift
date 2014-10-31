f8c5d5db6b5a698b6f510034d3b9f0142b30a4d3
Evgenii Neumerzhitckii <evgeny@bikeexchange.com.au>
2014-11-01 09:26:53 +1100

    Add controls

--- SpringAnimationCALayer/ViewController.swift
+++ SpringAnimationCALayer/ViewController.swift
@@ -11,12 +11,28 @@ import UIKit
 class ViewController: UIViewController {
   @IBOutlet weak var viewOneContainer: UIView!
 
+  let objectSize: CGFloat = 50
+
   override func viewDidLoad() {
     super.viewDidLoad()
 
 
-    let objectOne = UIView(frame: CGRect(origin: CGPoint: size: CGSize(x: 50, y: 50))
-//    viewOneContainer
+    let objectOne = UIView(frame: CGRect(origin: CGPoint(),
+      size: CGSize(width: objectSize, height: objectSize)))
+
+    objectOne.backgroundColor = UIColor.blueColor()
+    viewOneContainer.addSubview(objectOne)
+
+    UIView.animateWithDuration(2,
+      delay: 0,
+      usingSpringWithDamping: 1,
+      initialSpringVelocity: 1,
+      options: nil,
+      animations: {
+        let newCenterX = self.viewOneContainer.bounds.width / 2
+        objectOne.center = CGPoint(x: newCenterX, y: objectOne.center.y)
+      },
+      completion: nil)
   }
 
 
