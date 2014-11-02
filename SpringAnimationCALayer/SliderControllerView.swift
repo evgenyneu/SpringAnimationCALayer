188898746933358e868997f461179d45d9a4cda6
Evgenii Neumerzhitckii <evgeny@bikeexchange.com.au>
2014-11-02 11:33:19 +1100

    Redefine the meaning of controls

---
+++ SpringAnimationCALayer/SliderControllerView.swift
@@ -0,0 +1,23 @@
+//
+//  SliderControllerView.swift
+//  SpringAnimationCALayer
+//
+//  Created by Evgenii Neumerzhitckii on 2/11/2014.
+//  Copyright (c) 2014 Evgenii Neumerzhitckii. All rights reserved.
+//
+
+import UIKit
+
+class SliderControllerView: UIView {
+  init(name: String, defaults: SliderDefaults) {
+    super.init()
+  }
+
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+  }
+
+  required init(coder aDecoder: NSCoder) {
+      fatalError("init(coder:) has not been implemented")
+  }
+}
--- SpringAnimationCALayer/ViewController.swift
+++ SpringAnimationCALayer/ViewController.swift
@@ -15,41 +15,45 @@ class ViewController: UIViewController {
   let objectSize: CGFloat = 50
   let objectMargin: CGFloat = 10
 
-  // Duration
-  @IBOutlet weak var durationContainer: UIView!
-  @IBOutlet weak var durationLabel: UILabel!
-  @IBOutlet weak var durationSlider: UISlider!
-  let durationSliderDefaults = SliderDefaults(value: 3, minimumValue: 0.01, maximumValue: 5.0)
+  private let controlsData = [
+    "Duration": SliderDefaults(value: 3, minimumValue: 0.01, maximumValue: 5.0)
+  ]
 
-  // Damping
-  @IBOutlet weak var dampingContainer: UIView!
-  @IBOutlet weak var dampingLabel: UILabel!
-  @IBOutlet weak var dampingSlider: UISlider!
-  let dampingSliderDefaults = SliderDefaults(value: 0.1, minimumValue: 0.01, maximumValue: 0.6)
-
-  // Initial velocity
-  @IBOutlet weak var initialVelocityContainer: UIView!
-  @IBOutlet weak var initialVelocityLabel: UILabel!
-  @IBOutlet weak var initialVelocitySlider: UISlider!
-  let initialVelocutySliderDefaults = SliderDefaults(value: 1, minimumValue: 0.01, maximumValue: 10.0)
-
-  // Damping multiplier
-  @IBOutlet weak var dampingMultiplierContains: UIView!
-  @IBOutlet weak var dampingMultiplierLabel: UILabel!
-  @IBOutlet weak var dampingMultiplierSlider: UISlider!
-  let dampingMultiplierSliderDefaults = SliderDefaults(value: 9.5, minimumValue: 9.4, maximumValue: 9.6)
-
-  // Velocity multiplier
-  @IBOutlet weak var velocityMultiplierContains: UIView!
-  @IBOutlet weak var velocityMultiplierLabel: UILabel!
-  @IBOutlet weak var velocityMultiplierSlider: UISlider!
-  let velocityMultiplierSliderDefaults = SliderDefaults(value: 9.2, minimumValue: 7, maximumValue: 12)
-
-  // A
-  @IBOutlet weak var aMultiplierContains: UIView!
-  @IBOutlet weak var aMultiplierLabel: UILabel!
-  @IBOutlet weak var aMultiplierSlider: UISlider!
-  let aMultiplierSliderDefaults = SliderDefaults(value: 10, minimumValue: 5, maximumValue: 100)
+//  // Duration
+//  @IBOutlet weak var durationContainer: UIView!
+//  @IBOutlet weak var durationLabel: UILabel!
+//  @IBOutlet weak var durationSlider: UISlider!
+//  let durationSliderDefaults = SliderDefaults(value: 3, minimumValue: 0.01, maximumValue: 5.0)
+//
+//  // Damping
+//  @IBOutlet weak var dampingContainer: UIView!
+//  @IBOutlet weak var dampingLabel: UILabel!
+//  @IBOutlet weak var dampingSlider: UISlider!
+//  let dampingSliderDefaults = SliderDefaults(value: 0.1, minimumValue: 0.01, maximumValue: 0.6)
+//
+//  // Initial velocity
+//  @IBOutlet weak var initialVelocityContainer: UIView!
+//  @IBOutlet weak var initialVelocityLabel: UILabel!
+//  @IBOutlet weak var initialVelocitySlider: UISlider!
+//  let initialVelocutySliderDefaults = SliderDefaults(value: 1, minimumValue: 0.01, maximumValue: 10.0)
+//
+//  // Damping multiplier
+//  @IBOutlet weak var dampingMultiplierContains: UIView!
+//  @IBOutlet weak var dampingMultiplierLabel: UILabel!
+//  @IBOutlet weak var dampingMultiplierSlider: UISlider!
+//  let dampingMultiplierSliderDefaults = SliderDefaults(value: 9.5, minimumValue: 9.4, maximumValue: 9.6)
+//
+//  // Velocity multiplier
+//  @IBOutlet weak var velocityMultiplierContains: UIView!
+//  @IBOutlet weak var velocityMultiplierLabel: UILabel!
+//  @IBOutlet weak var velocityMultiplierSlider: UISlider!
+//  let velocityMultiplierSliderDefaults = SliderDefaults(value: 9.2, minimumValue: 7, maximumValue: 12)
+//
+//  // A
+//  @IBOutlet weak var aMultiplierContains: UIView!
+//  @IBOutlet weak var aMultiplierLabel: UILabel!
+//  @IBOutlet weak var aMultiplierSlider: UISlider!
+//  let aMultiplierSliderDefaults = SliderDefaults(value: 10, minimumValue: 5, maximumValue: 100)
 
 
   var objectOne: UIView!
@@ -64,29 +68,37 @@ class ViewController: UIViewController {
     createObjectOne()
     createObjectTwo()
 
+    createControls()
+
     // Duration
-    SliderDefaults.set(durationSlider, defaults: durationSliderDefaults)
-    onDurationSliderChanged(durationSlider)
+//    SliderDefaults.set(durationSlider, defaults: durationSliderDefaults)
+//    onDurationSliderChanged(durationSlider)
+//
+//    // Damping
+//    SliderDefaults.set(dampingSlider, defaults: dampingSliderDefaults)
+//    onDampingSliderChanged(dampingSlider)
+//
+//    // Initial velocity
+//    SliderDefaults.set(initialVelocitySlider, defaults: initialVelocutySliderDefaults)
+//    onInitialVelocitySliderChanged(initialVelocitySlider)
+//
+//    // Damping multiplier
+//    SliderDefaults.set(dampingMultiplierSlider, defaults: dampingMultiplierSliderDefaults)
+//    onDampingMultiplierSliderChanged(dampingMultiplierSlider)
+//
+//    // Velocity multiplier
+//    SliderDefaults.set(velocityMultiplierSlider, defaults: velocityMultiplierSliderDefaults)
+//    onVelocityMultiplierSliderChanged(velocityMultiplierSlider)
+//
+//    // A
+//    SliderDefaults.set(aMultiplierSlider, defaults: aMultiplierSliderDefaults)
+//    onAMultiplierSliderChanged(aMultiplierSlider)
+  }
 
-    // Damping
-    SliderDefaults.set(dampingSlider, defaults: dampingSliderDefaults)
-    onDampingSliderChanged(dampingSlider)
-
-    // Initial velocity
-    SliderDefaults.set(initialVelocitySlider, defaults: initialVelocutySliderDefaults)
-    onInitialVelocitySliderChanged(initialVelocitySlider)
-
-    // Damping multiplier
-    SliderDefaults.set(dampingMultiplierSlider, defaults: dampingMultiplierSliderDefaults)
-    onDampingMultiplierSliderChanged(dampingMultiplierSlider)
-
-    // Velocity multiplier
-    SliderDefaults.set(velocityMultiplierSlider, defaults: velocityMultiplierSliderDefaults)
-    onVelocityMultiplierSliderChanged(velocityMultiplierSlider)
-
-    // A
-    SliderDefaults.set(aMultiplierSlider, defaults: aMultiplierSliderDefaults)
-    onAMultiplierSliderChanged(aMultiplierSlider)
+  private func createControls() {
+    for (name, defaults) in controlsData {
+      controlsContainer.addSubview(SliderControllerView(name: name, defaults: defaults))
+    }
   }
 
   private func createObjectOne() {
@@ -117,12 +129,12 @@ class ViewController: UIViewController {
     objectsContainer.backgroundColor = nil
 
     controlsContainer.backgroundColor = nil
-    durationContainer.backgroundColor = nil
-    dampingContainer.backgroundColor = nil
-    initialVelocityContainer.backgroundColor = nil
-    dampingMultiplierContains.backgroundColor = nil
-    velocityMultiplierContains.backgroundColor = nil
-    aMultiplierContains.backgroundColor = nil
+//    durationContainer.backgroundColor = nil
+//    dampingContainer.backgroundColor = nil
+//    initialVelocityContainer.backgroundColor = nil
+//    dampingMultiplierContains.backgroundColor = nil
+//    velocityMultiplierContains.backgroundColor = nil
+//    aMultiplierContains.backgroundColor = nil
   }
 
   private func animate() {
@@ -134,33 +146,35 @@ class ViewController: UIViewController {
     objectOne.frame.origin = CGPoint(x: 0, y: 0)
     objectOne.layer.removeAllAnimations()
 
-    UIView.animateWithDuration(NSTimeInterval(durationSlider.value),
-      delay: 0,
-      usingSpringWithDamping: CGFloat(dampingSlider.value),
-      initialSpringVelocity: CGFloat(initialVelocitySlider.value),
-      options: UIViewAnimationOptions.BeginFromCurrentState,
-      animations: {
-        let newCenterY = self.objectsContainer.bounds.height / 2
-        self.objectOne.frame.origin = CGPoint(x: 0, y: newCenterY)
-      },
-      completion: nil)
+//    UIView.animateWithDuration(NSTimeInterval(durationSlider.value),
+//      delay: 0,
+//      usingSpringWithDamping: CGFloat(dampingSlider.value),
+//      initialSpringVelocity: CGFloat(initialVelocitySlider.value),
+//      options: UIViewAnimationOptions.BeginFromCurrentState,
+//      animations: {
+//        let newCenterY = self.objectsContainer.bounds.height / 2
+//        self.objectOne.frame.origin = CGPoint(x: 0, y: newCenterY)
+//      },
+//      completion: nil)
+//
+//    println("Initial velocity \(CGFloat(initialVelocitySlider.value))")
   }
 
   private func animateObjectTwo() {
     setObjectTwoPosition(objectsContainer.bounds.height / 2)
 
-    let dampingMultiplier = Double(dampingMultiplierSlider.value)
-    let velocityMultiplier = Double(velocityMultiplierSlider.value)
-
-    let values = springValues(0, toValue: Double(objectsContainer.bounds.height / 2),
-      damping: dampingMultiplier * Double(dampingSlider.value),
-      initialVelocity: velocityMultiplier * Double(initialVelocitySlider.value))
-
-    let animation = CAKeyframeAnimation(keyPath: "position.y")
-    animation.values = values
-    animation.duration = CFTimeInterval(durationSlider.value)
-    objectTwo.layer.removeAllAnimations()
-    objectTwo.layer.addAnimation(animation, forKey: "mySpringAnimation")
+//    let dampingMultiplier = Double(dampingMultiplierSlider.value)
+//    let velocityMultiplier = Double(velocityMultiplierSlider.value)
+//
+//    let values = springValues(0, toValue: Double(objectsContainer.bounds.height / 2),
+//      damping: dampingMultiplier * Double(dampingSlider.value),
+//      initialVelocity: velocityMultiplier * Double(initialVelocitySlider.value))
+//
+//    let animation = CAKeyframeAnimation(keyPath: "position.y")
+//    animation.values = values
+//    animation.duration = CFTimeInterval(durationSlider.value)
+//    objectTwo.layer.removeAllAnimations()
+//    objectTwo.layer.addAnimation(animation, forKey: "mySpringAnimation")
   }
 
   private func springValues(fromValue: Double, toValue: Double,
@@ -184,8 +198,9 @@ class ViewController: UIViewController {
 
   private func springValueNormalized(x: Double, damping: Double, initialVelocity: Double) -> Double {
 
-    let A = Double(aMultiplierSlider.value)
-    return pow(M_E, -2.6 * pow(damping, 0.1) * x) * cos(A * pow(initialVelocity,0.1) * x / damping)
+//    let A = Double(aMultiplierSlider.value)
+//    return pow(M_E, -2.6 * pow(damping, 0.1) * x) * cos(A * pow(initialVelocity,0.1) * x / damping)
+    return 0
   }
 
   @IBAction func onGoTapped(sender: AnyObject) {
@@ -196,41 +211,41 @@ class ViewController: UIViewController {
     animate()
   }
 
-  @IBAction func onDurationSliderChanged(sender: AnyObject) {
-    if let slider = sender as? UISlider {
-      updateSliderLabel(slider, label: durationLabel, caption: "Duration")
-    }
-  }
-
-  @IBAction func onDampingSliderChanged(sender: AnyObject) {
-    if let slider = sender as? UISlider {
-      updateSliderLabel(slider, label: dampingLabel, caption: "Damping")
-    }
-  }
-
-  @IBAction func onInitialVelocitySliderChanged(sender: AnyObject) {
-    if let slider = sender as? UISlider {
-      updateSliderLabel(slider, label: initialVelocityLabel, caption: "Initial velocity")
-    }
-  }
-
-  @IBAction func onDampingMultiplierSliderChanged(sender: AnyObject) {
-    if let slider = sender as? UISlider {
-      updateSliderLabel(slider, label: dampingMultiplierLabel, caption: "Damping multiplier")
-    }
-  }
-
-  @IBAction func onVelocityMultiplierSliderChanged(sender: AnyObject) {
-    if let slider = sender as? UISlider {
-      updateSliderLabel(slider, label: velocityMultiplierLabel, caption: "Velocity multiplier")
-    }
-  }
-
-  @IBAction func onAMultiplierSliderChanged(sender: AnyObject) {
-    if let slider = sender as? UISlider {
-      updateSliderLabel(slider, label: aMultiplierLabel, caption: "A")
-    }
-  }
+//  @IBAction func onDurationSliderChanged(sender: AnyObject) {
+//    if let slider = sender as? UISlider {
+//      updateSliderLabel(slider, label: durationLabel, caption: "Duration")
+//    }
+//  }
+//
+//  @IBAction func onDampingSliderChanged(sender: AnyObject) {
+//    if let slider = sender as? UISlider {
+//      updateSliderLabel(slider, label: dampingLabel, caption: "Damping")
+//    }
+//  }
+//
+//  @IBAction func onInitialVelocitySliderChanged(sender: AnyObject) {
+//    if let slider = sender as? UISlider {
+//      updateSliderLabel(slider, label: initialVelocityLabel, caption: "Initial velocity")
+//    }
+//  }
+//
+//  @IBAction func onDampingMultiplierSliderChanged(sender: AnyObject) {
+//    if let slider = sender as? UISlider {
+//      updateSliderLabel(slider, label: dampingMultiplierLabel, caption: "Damping multiplier")
+//    }
+//  }
+//
+//  @IBAction func onVelocityMultiplierSliderChanged(sender: AnyObject) {
+//    if let slider = sender as? UISlider {
+//      updateSliderLabel(slider, label: velocityMultiplierLabel, caption: "Velocity multiplier")
+//    }
+//  }
+//
+//  @IBAction func onAMultiplierSliderChanged(sender: AnyObject) {
+//    if let slider = sender as? UISlider {
+//      updateSliderLabel(slider, label: aMultiplierLabel, caption: "A")
+//    }
+//  }
 
   private func updateSliderLabel(slider: UISlider, label: UILabel, caption: String) {
     label.text = "\(caption): \(formatValue(slider.value))"
